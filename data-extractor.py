67aba89a2ea5ff075fa2a29e192f896c0203afbf
agusti <agustigm@gmail.com>
2024-05-04 09:57:23 +0200

    fixes

--- data-extractor.py
+++ data-extractor.py
@@ -6,10 +6,10 @@ with open('inditextech_hackupc_challenge_images.csv') as file:
     data = csv.reader(file)
     for idx, element in enumerate(data):
         if 0 < idx < 2:
-            os.mkdir(idx)
+            os.mkdir(str(idx))
             for i, url in enumerate(element):
                 image = requests.get(url)
-                with open(i+'fle.jpg', 'wb') as f:
+                with open(f'{str(idx)}{os.sep}image{i}.jpg', 'wb') as f:
                     f.write(image.content)
 
 
