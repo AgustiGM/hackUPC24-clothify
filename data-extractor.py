2c04940c75d9ec9b6aae3a257bd9431b1575d7ec
agusti <agustigm@gmail.com>
2024-05-04 10:01:08 +0200

    Refinements

--- data-extractor.py
+++ data-extractor.py
@@ -1,15 +1,19 @@
 import requests
 import os
+import shutil
 import csv
 
 with open('inditextech_hackupc_challenge_images.csv') as file:
     data = csv.reader(file)
     for idx, element in enumerate(data):
         if 0 < idx < 2:
-            os.mkdir(str(idx))
+            directory = f'{str(idx)}'
+            if os.path.exists(directory):
+                shutil.rmtree(directory)
+            os.makedirs(directory)
             for i, url in enumerate(element):
                 image = requests.get(url)
-                with open(f'{str(idx)}{os.sep}image{i}.jpg', 'wb') as f:
+                with open(f'.{os.sep}{directory}{os.sep}image{i}.jpg', 'wb') as f:
                     f.write(image.content)
 
 
